30e3986b5d96cf8a4a41b98bdead4fbed92bdebe
ericpkatz <ericpkatz@gmail.com>
2022-03-01 19:31:11 -0500

    finished app

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ Task.generateRandom = function(){
 
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
 
 app.get('/api/tasks', async(req, res, next) => {
   try {
