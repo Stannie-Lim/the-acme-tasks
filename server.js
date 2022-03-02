a9d851b465c80f84ea505eb6bc17cfd32721991c
ericpkatz <ericpkatz@gmail.com>
2022-03-01 19:14:03 -0500

    set of api routes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//TODO move to db folder
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/the-acme-task-db');
 
@@ -11,6 +12,38 @@ Task.generateRandom = function(){
   return this.create({ name: `Task ${ Math.ceil(Math.random()*5000)}`});
 }
 
+const express = require('express');
+const app = express();
+
+app.get('/api/tasks', async(req, res, next) => {
+  try {
+    res.send(await Task.findAll());
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.post('/api/tasks', async(req, res, next)=> {
+  try {
+    res.status(201).send(await Task.generateRandom());
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.delete('/api/tasks/:id', async(req, res, next) => {
+  try {
+    const task = await Task.findByPk(req.params.id);
+    await task.destroy();
+    res.sendStatus(204);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
 const start = async()=> {
   try {
     await sequelize.sync({ force: true });
@@ -19,6 +52,9 @@ const start = async()=> {
       Task.generateRandom(),
       Task.generateRandom()
     ]);
+
+    const port = process.env.PORT || 3000;
+    app.listen(port, ()=> console.log(`listening on port ${port}`));
   }
   catch(ex){
     console.log(ex);
